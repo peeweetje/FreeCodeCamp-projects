caf29b9632984cf58a14f41a0cbf0dd6b18dea0a
Petra Vos <petravos70@gmail.com>
2018-09-10 20:23:35 +0200

    add drum data. add props. change code

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,8 +1,72 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const drumdata = [
+  {
+    keyCode: 81,
+    keyPad: "Q",
+    id: "Heater-1",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+  {
+    keyCode: 87,
+    keyPad: "W",
+    id: "Heater-2",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  },
+  {
+    keyCode: 69,
+    keyPad: "E",
+    id: "Heater-3",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+  {
+    keyCode: 65,
+    keyPad: "A",
+    id: "Heater-4",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+  {
+    keyCode: 83,
+    keyPad: "S",
+    id: "Clap",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    keyCode: 68,
+    keyPad: "D",
+    id: "Open-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+  {
+    keyCode: 90,
+    keyPad: "Z",
+    id: "Kick-n'-Hat",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+  {
+    keyCode: 88,
+    keyPad: "X",
+    id: "Kick",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    keyCode: 67,
+    keyPad: "C",
+    id: "Closed-HH",
+    src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
+];
+
 class App extends Component {
-  handleClick = () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Drum machine"
+    };
+  }
+
+  handleSound = () => {
     this.audio.play();
   };
 
@@ -10,24 +74,30 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Drum machine</h1>
+          <h1>{this.state.title}</h1>
         </header>
+
         <div className="container" id="drum-machine">
           <div id="display" />
-          <div className="drum-pad" id="Q" onClick={this.handleClick()}>
+
+          <div
+            className="drum-pad"
+            id={this.props.keyPad}
+            onClick={this.handleSound}
+          >
             <p>Q</p>
             <audio
               ref={ref => (this.audio = ref)}
-              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+              src={this.props.src}
               className="clip"
-              id="Q"
+              id={this.props.keyPad}
             />
           </div>
-          <div className="drum-pad" id="w" onClick={this.handleClick()}>
+          <div className="drum-pad" id="w" onClick={this.handleSound}>
             <p>W</p>
             <audio
               ref={ref => (this.audio = ref)}
-              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+              src={this.props.src}
               className="clip"
               id="w"
             />
