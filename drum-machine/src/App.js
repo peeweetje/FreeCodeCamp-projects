609942df665ec43fef3e168f0f8deed0294daa5f
Petra Vos <petravos70@gmail.com>
2018-09-11 20:06:00 +0200

    remove code no longer relevant. access drumdata

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -68,6 +68,7 @@ class App extends Component {
 
   handleSound = () => {
     this.audio.play();
+    //console.log("clicked");
   };
 
   render() {
@@ -79,92 +80,24 @@ class App extends Component {
 
         <div className="container" id="drum-machine">
           <div id="display" />
-
-          <div
-            className="drum-pad"
-            id={this.props.keyPad}
-            onClick={this.handleSound}
-          >
-            <p>Q</p>
-            <audio
-              ref={ref => (this.audio = ref)}
-              src={this.props.src}
-              className="clip"
-              id={this.props.keyPad}
-            />
-          </div>
-          <div className="drum-pad" id="w" onClick={this.handleSound}>
-            <p>W</p>
-            <audio
-              ref={ref => (this.audio = ref)}
-              src={this.props.src}
-              className="clip"
-              id="w"
-            />
-          </div>
-          <div className="drum-pad" id="e">
-            <p>E</p>
-            <audio
-              ref={ref => (this.audio = ref)}
-              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-              className="clip"
-              id="e"
-            />
-          </div>
-          <div className="drum-pad" id="a">
-            <p>A</p>
-            <audio
-              ref={ref => (this.audio = ref)}
-              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-              className="clip"
-              id="a"
-            />
-          </div>
-          <div className="drum-pad" id="s">
-            <p>S</p>
-            <audio
-              ref={ref => (this.audio = ref)}
-              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
-              className="clip"
-              id="s"
-            />
-          </div>
-          <div className="drum-pad" id="d">
-            <p>D</p>
-            <audio
-              ref={ref => (this.audio = ref)}
-              src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-              className="clip"
-              id="d"
-            />
-          </div>
-          <div className="drum-pad" id="z">
-            <p>Z</p>
-            <audio
-              ref={ref => (this.audio = ref)}
-              src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-              className="clip"
-              id="z"
-            />
-          </div>
-          <div className="drum-pad" id="x">
-            <p>X</p>
-            <audio
-              ref={ref => (this.audio = ref)}
-              src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-              className="clip"
-              id="x"
-            />
-          </div>
-          <div className="drum-pad" id="c">
-            <p>C</p>
-            <audio
-              ref={ref => (this.audio = ref)}
-              src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-              className="clip"
-              id="c"
-            />
-          </div>
+          {drumdata.map(data => (
+            <div
+              className="drum-pad"
+              id={data.keyPad}
+              onClick={this.handleSound}
+              key={data.id}
+            >
+              <p>{data.keyPad}</p>
+              <audio
+                className="clip"
+                ref={ref => (this.audio = ref)}
+                src={data.src}
+                id={data.keyPad}
+                key={data.id}
+              />
+            </div>
+          ))}
+          ;
         </div>
       </div>
     );
