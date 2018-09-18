15fe3187c48d9ebdba2576abb7a15abdb018d9ec
Petra Vos <petravos70@gmail.com>
2018-09-18 19:55:28 +0200

    Add the keyPress function

--- drum-machine/src/Drumpad.js
+++ drum-machine/src/Drumpad.js
@@ -5,6 +5,23 @@ export default class Drumpad extends Component {
     this.audio.play();
   };
 
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleKeyPress = e => {
+    // Compare keyCode with keyPad. charCodeAt changes the keypad in a number.
+    // For example, if we click the 'W' drumpad, e.keyCode will be 87, and this.props.keyPad will be "W".
+    // charCodeAt() will convert the letter to the keyCode, so this.props.keyPad.charCodeAt() will be 87.
+    if (e.keyCode === this.props.keyPad.charCodeAt()) {
+      this.audio.play();
+    }
+  };
+
   render() {
     return (
       <div className="drum-pad" id={this.props.id} onClick={this.handleSound}>
@@ -14,6 +31,7 @@ export default class Drumpad extends Component {
           className="clip"
           src={this.props.src}
           ref={ref => (this.audio = ref)}
+          onKeyPress={this.handleKeyPress}
         />
       </div>
     );
